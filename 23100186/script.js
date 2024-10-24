980e9ba4ee493b86bbf29ea75873654e23e28497
m0ndr9g0n <ivan.k.mondragon@gmail.com>
2024-10-24 16:54:24 -0500

    pagina
    web

---
+++ 23100186/script.js
@@ -0,0 +1,7 @@
+const boton = document.getElementById('cambiar-color');
+//function cambiarColor(){
+  //  document.body.style.backgroundColor = 'pink';
+//}
+bonton.addEventListener('click' , () => (){
+    document.body.style.backgroundColor = 'pink';
+})
